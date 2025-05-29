6dfa990566eaf83a54e0415c156415edcae97964
Jefersonlopezr <jefersonlopezr99@gmail.com>
2025-05-29 17:53:47 -0500

    feat: :art: Create fucntion to registrar_dueno and registrar_mascota

--- script.js
+++ script.js
@@ -47,3 +47,47 @@ function mostrar_menu() {
         }
     } while (opcion !== "6");
 }
+
+function registrar_dueno() {
+    const ID = duenos.length + 1;
+    alert("Tu ID autogenerado es: " + ID);
+    const nombre = prompt("Ingrese el nombre del dueno");
+    if (!ID || !nombre || nombre.trim() === "") {
+        alert("Esta información es inválida o te falta un campo por llenar. Por favor intente nuevamente");
+        return;
+    }
+    duenos.push({ ID: ID, nombre: nombre });
+    alert("El dueno se registró exitosamente")
+}
+
+function registrar_mascota() {
+    const ID = prompt("Ingrese el ID de tu mascota");
+    const nombre = prompt("Ingrese el nombre de la mascota:");
+    const especie = prompt("Ingrese la especie (Perro, Gato, Ave, otro etc.):");
+    const edad = parseInt(prompt("Ingrese la edad de la mascota (en años):"));
+    const peso = parseFloat(prompt("Ingrese el peso de la mascota (en kg):"));
+    const estadoSalud = prompt("Ingrese el estado de salud (Sano, Enfermo, En tratamiento):");
+    const ID_dueno = parseInt(prompt("Ingrese el ID del dueno de la mascota"));
+
+
+    const dueno = duenos.find(d => d.ID === ID_dueno);
+    if (!dueno) {
+        alert("dueno no registrado. Registre primero al dueno");
+        return;
+    }
+    if (!nombre || !especie || isNaN(edad) || isNaN(peso) || !estadoSalud) {
+        alert("Información inválida. Intente nuevamente");
+        return;
+    }
+    const mascota = {
+        ID,
+        nombre,
+        especie,
+        edad,
+        peso,
+        estadoSalud,
+        ID_dueno
+    };
+    mascotas.push(mascota);
+    alert("Mascota registrada exitosamente.");
+}
